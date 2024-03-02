dd5972114c10674e1a9a3186086ad3cb611f4964
Abraham <abrahamnhier97@gmail.com>
2024-03-02 13:47:23 +0300

    finished setting up the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import { FcMenu } from "react-icons/fc";
+import { Link,useLocation } from "react-router-dom"
+
 
 const NavBar = () => {
+    const { pathname } = useLocation();
   return (
     <nav className="fixed top-0 left-0 flex justify-between items-center z-10 p-4 sm:p-16 bg-gray-800 w-full">
       <h1 className="text-4xl cursor-pointer">Portfolio</h1>
       <FcMenu className="text-5xl text-white hidden"/>
         <ul className="flex list-none">
-            <li><a href="#about" className="text-2xl ml-16">About</a></li>
-            <li><a href="#projects" className="text-2xl ml-16">Projects</a></li>
-            <li><a href="#contact" className="text-2xl ml-16">Contact</a></li>
+           <NavItem to="/" currentPath={pathname}>
+            About
+           </NavItem>
+           <NavItem to="/projects" currentPath={pathname}>
+            Projects
+           </NavItem>
+           <NavItem to="/contacts" currentPath={pathname}>
+            Contacts
+           </NavItem>
         </ul>
        
     </nav>
   );
 };
+const NavItem = ({ to, currentPath, children}) => (
+    <li className="relative px-10">
+      <Link className="text-white text-3xl" t0={to}>
+      {children}
+      </Link>  
+      <div className={`${currentPath === to ? "w-1/2": "w-0"} h-0.3rem bg-gray-500 absolute bottom-0 left-60 sm:left-0 transition-all duration-750`}>
+
+      </div>
+    </li>
+)
 
 export default NavBar;
