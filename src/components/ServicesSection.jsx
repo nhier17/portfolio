57e86dc7ff18e819998f93e3571450e83516032d
Abraham <abrahamnhier97@gmail.com>
2024-03-03 18:52:41 +0300

    styling

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,33 +1,36 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React from 'react';
+import { FaCode, FaHashtag, FaChartLine } from 'react-icons/fa';
+
 
 const ServicesSection = () => {
   return (
     <div>
-        <h2>My <span>Services</span></h2>
-        <div className="container">
-            <div className="box">
-            <FaCode />
-            <h3>Web Development</h3>
-            <p>Creating applications and elegant website designs suited for your needs</p>
-            </div>
+    <h2 className="mb-8 text-[5rem] text-center">My <span className="text-[#0ef]">Services</span></h2>
+    <div className="services-container">
+      <div className="service-item">
+        <div className="bg-[#323946] p-12 pt-8 pb-16 text-center border-solid border-2 border-[#0ef]">
+          <FaCode className="text-center text-[7rem] text-[#0ef] m-auto"/>
+          <h3 className="text-3xl p-4">Web Development</h3>
+          <p className="text-xl">Creating applications and elegant website designs suited for your needs</p>
         </div>
-        <div className="container">
-            <div className="box">
-            <FaCode />
-            <h3>Web Development</h3>
-            <p>Creating applications and elegant website designs suited for your needs</p>
-            </div>
+      </div>
+      <div className="service-item">
+        <div className="bg-[#323946] p-12 pt-8 pb-16 text-center">
+          <FaHashtag className="text-center text-[7rem] text-[#0ef] m-auto"/>
+          <h3 className="text-3xl p-4">Code Your Dream Project</h3>
+          <p className="text-xl">Do you have an idea for your next great website? Let's make it a reality.</p>
         </div>
-        <div className="container">
-            <div className="box">
-            <FaCode />
-            <h3>Web Development</h3>
-            <p>Creating applications and elegant website designs suited for your needs</p>
-            </div>
+      </div>
+      <div className="service-item">
+        <div className="bg-[#323946] p-12 pt-8 pb-16 text-center border-solid border-2 border-[#0ef]">
+          <FaChartLine className="text-center text-[7rem] text-[#0ef] m-auto"/>
+          <h3 className="text-3xl p-4">Analytics</h3>
+          <p className="text-xl">Utilize data to drive insights and improve your online presence</p>
         </div>
+      </div>
     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default ServicesSection
+export default ServicesSection;
